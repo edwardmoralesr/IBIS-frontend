0a5e2ae7b41750dc809806b93423774dc1721dd0
root <root@Edward>
2026-04-11 18:14:46 -0500

    Menú diseño

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,23 +1,15 @@
-import { useState } from "react";
-import { Home24Regular } from "@fluentui/react-icons";
 import { useNavigate } from "react-router-dom";
+import { Home24Regular } from "@fluentui/react-icons";
 import "./SideBar.css";
 
 export default function Sidebar() {
-  const [open, setOpen] = useState(false);
   const navigate = useNavigate();
 
   return (
-    <div
-      className={`sidebar ${open ? "open" : "closed"}`}
-      onMouseEnter={() => setOpen(true)}
-      onMouseLeave={() => setOpen(false)}
-    >
-      <div className="menu">
-        <div className="menu-item" onClick={() => navigate("/inicio")}>
-          <Home24Regular />
-          {open && <span>Inicio</span>}
-        </div>
+    <div className="sidebar">
+      <div className="menu-item" onClick={() => navigate("/inicio")}>
+        <Home24Regular className="icon" />
+        <span className="label">Inicio</span>
       </div>
     </div>
   );
